579d529ec52437fb746d239c2faee99175d5e300
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-13 18:04:23 +0900

    ビューファイルの作成・追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :purchases
   with_options presence: true do
     validates :nickname
-    validates :email,          
+    validates :email
     validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
     validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
