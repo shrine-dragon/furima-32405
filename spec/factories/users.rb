f6b7d96d18bf121578556846c1ecef7590a90d66
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-14 10:21:17 +0900

    テストコードの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {Faker::Name}
+    email                 {Faker:Internet.free_email}
+    password              {Faker:Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {Faker::First_name}
+    first_name            {Faker::Last_name}
+    last_name_kana        {Faker::First_name_kana}
+    first_name_kana       {Faker::Last_name_kana}
+    birth_day             {Faker::Birth_day}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do 
+    it 'nicknameが空では登録できない' do
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      binding.pry
+    end
+    it 'emailが空では登録できない' do
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'password_confirmationが空では登録できない' do
+    end
+    it 'last_nameが空では登録できない' do
+    end
+    it 'first_nameが空では登録できない' do
+    end
+    it 'last_name_kanaが空では登録できない' do
+    end
+    it 'first_name_kanaが空では登録できない' do
+    end
+    it 'birth_dayが空では登録できない' do
+    end
+  end
 end
