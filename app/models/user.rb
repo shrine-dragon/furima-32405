0d2ec1402644ac13135e2fb61a92721cf8fd9567
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-14 17:40:41 +0900

    userモデルの重複した部分を編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,9 @@ class User < ApplicationRecord
   has_many :purchases
   with_options presence: true do
     validates :nickname
-    validates :password,            format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers." }
-    validates :last_name,           format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name_kana,      format: { with: /\A[ァ-ヶー－]+\z/,   message: "is invalid. Input full-width katakana characters."}
-    validates :first_name_kana,     format: { with: /\A[ァ-ヶー－]+\z/,   message: "is invalid. Input full-width katakana characters."}
+    validates :password,                              format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers." }
+    validates :last_name, :first_name,                format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :last_name_kana, :first_name_kana,      format: { with: /\A[ァ-ヶー－]+\z/,   message: "is invalid. Input full-width katakana characters."}
     validates :birth_day
   end
 end
