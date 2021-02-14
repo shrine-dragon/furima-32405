ce977df1e1e7192b0cb65e018ac1da5665f128f9
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-14 11:13:34 +0900

    userモデルの編集2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
   has_many :purchases
   with_options presence: true do
     validates :nickname
-    validates :email
-    validates :encrypted_password,  format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
+    validates :email,               format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is invalid. Input half-width characters."},
+                                    uniqueness: true
+    validates :encrypted_password,  format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."},
+                                    length: { minimum: 6 }, confirmation: true
     validates :last_name,           format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :first_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :last_name_kana,      format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
