0370adfad64908b3af7213e23beefbdbe9b1ce63
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-14 10:28:58 +0900

    カラムの順番入れ替え

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email
-    validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
-    validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name_kana,    format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :last_name_kana,     format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :encrypted_password,  format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
+    validates :last_name,           format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :last_name_kana,      format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana,     format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :birth_day
   end
 end
