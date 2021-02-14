fad4e889c82ca9eb6f9473027c2a0b16c8ed3b82
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-14 11:46:39 +0900

    テストコードの実装準備3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,18 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name}
-    email                 {Faker:Internet.free_email}
-    password              {Faker:Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {Faker::First_name}
-    first_name            {Faker::Last_name}
-    last_name_kana        {Faker::First_name_kana}
-    first_name_kana       {Faker::Last_name_kana}
-    birth_day             {Faker::Birth_day}
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+
+    transient do 
+      person { Gimei.name }
+    end
+
+    last_name             { person.last.kanji}
+    first_name            { person.first.kanji }
+    last_name_kana        { person.last.katakana }
+    first_name_kana       { person.first.katakana }
+    birth_day             { Faker::Date.backward }
   end
 end
