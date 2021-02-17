43600fcfcf78a9cd0d05a0c5180e05bfeb914f6a
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-17 14:30:00 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,10 @@ class Item < ApplicationRecord
   has_one          :purechase
   has_one_attached :image
   belongs_to       :category, :quality, :burden_fee, :prefecture, :days_to_ship
+
+  validates :image, :name, :description, presence: true
+  
+  with_options numericality: { other_than: 1} do
+    validates :category_id, :quality_id, :burden_fee_id, :prefecture_id, :days_to_ship_id
+  end
 end
