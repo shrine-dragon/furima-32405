fd9481735bfc51435e1c212908c4ab68c0650439
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-17 14:19:56 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to       :user
+  has_one          :purechase
+  has_one_attached :image
+  belongs_to       :category, :quality, :burden_fee, :prefecture, :days_to_ship
 end
