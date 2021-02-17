2f41053abbfff9ac40005179e1ed5f20217a57c3
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-17 20:03:04 +0900

    販売価格入力による販売手数料・販売利益の表示の変化

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,14 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+const money_calc = () => {
+  const price = document.getElementById("item-price");
+  price.addEventListener("input", () => {
+    const priceValue = price.value;
+    // 販売手数料の計算 
+    const salesCommission = document.getElementById("add-tax-price");
+    salesCommission.innerHTML = Math.floor(priceValue / 10);
+    // 販売利益の計算
+    const salesProfit = document.getElementById("profit");
+    const commission = priceValue / 10;
+    salesProfit.innerHTML = Math.floor(priceValue - commission);
+  });
+};
+window.addEventListener("load", money_calc);
