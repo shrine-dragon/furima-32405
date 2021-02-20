f453d50730e34757964ba9309da43e5c3cada3e8
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-20 14:38:44 +0900

    モデルの正規表現の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
   # active_hashのデータ
-  with_options numericality: { other_than: 0 } do
+  with_options numericality: { other_than: 0, message: "must be other than 0" } do
     validates :category_id, :quality_id, :burden_fee_id, :days_to_ship_id, :prefecture_id
   end
 end
