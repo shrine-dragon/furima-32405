533705c99e9ae2af2121fd47f6daa180795b3fd3
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-20 12:37:56 +0900

    fakerダミーデータにおけるimageデータの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    image           { Faker::Avatar.image }
     name            { Faker::Commerce.product_name }
     description     { Faker::Lorem.sentence }
     category_id     { Faker::Number.between(from: 1, to: 10) }
@@ -9,5 +8,11 @@ FactoryBot.define do
     prefecture_id   { Faker::Number.between(from: 1, to: 47) }
     days_to_ship_id { Faker::Number.between(from: 1, to: 3) }
     price           { Faker::Number.between(from: 300, to: 9999999) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
