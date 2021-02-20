7fefe9af61101852dd9265a145b884eaa863e31a
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-20 11:58:08 +0900

    fakerでダミーデータを作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image           { Faker::Avatar.image }
+    name            { Faker::Commerce.product_name }
+    description     { Faker::Lorem.sentece }
+    category_id     { Faker::Number.between(from: 1, to: 10) }
+    quality_id      { Faker::Number.between(from: 1, to: 6) }
+    burden_fee_id   { Faker::Number.between(from: 1, to: 2) }
+    prefecture_id   { Faker::Number.between(from: 1, to: 47) }
+    days_to_ship_id { Faker::Number.between(from: 1, to: 3) }
+    price           { Faker::Number.between(from: 300, to: 9999999) }
   end
 end
