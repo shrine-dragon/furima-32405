378238d6d7a89bb2281d5bc129d2c31d61046f8b
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-20 12:06:26 +0900

    テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)    
+  end
+
+  describe '商品出品機能' do
+    context '商品出品がうまくいく時' do
+      it '全ての値が正しく入力されていれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品がうまくいかない時' do
+      
+    end
+  end
 end
