9a5027d6b236ca7ac97e4c46e7d6e6d1eb514fa7
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-24 19:33:23 +0900

    メソッド名の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,  except: [:index, :show]
-  before_action :set_item,            only:   [:show, :edit, :update]
-  before_action :not_use_action,      only:   [:edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item,           only:   [:show, :edit, :update]
+  before_action :move_to_root_path,  only:   [:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -43,7 +43,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def not_use_action
+  def move_to_root_path
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
