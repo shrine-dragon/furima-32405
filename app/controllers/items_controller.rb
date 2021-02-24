cdc6b7105951d92264b0213b79985347ef5a7391
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-24 16:45:36 +0900

    updateコントローラーの修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,9 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path
+    end
   end
 
   private
