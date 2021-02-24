03033ad97574ca02a3d362c32719cf5c83b4ba68
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-24 11:15:22 +0900

    アソシエーションを用いた記述に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,11 +20,6 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @category = Category.find_by(id: @item.category_id)
-    @quality = Quality.find_by(id: @item.quality_id)
-    @burden_fee = BurdenFee.find_by(id: @item.burden_fee_id)
-    @prefecture = Prefecture.find_by(id: @item.prefecture_id)
-    @days_to_ship = DaysToShip.find_by(id: @item.days_to_ship_id)
   end
 
   private
