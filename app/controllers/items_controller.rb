6a7eeec6e1e97d5ddc3888518335fa9322cb06df
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-24 19:21:25 +0900

    itemsコントローラーの修正（可読性向上のため）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!,  except: [:index, :show]
+  before_action :set_item,            only:   [:show, :edit, :update]
+  before_action :not_use_action,      only:   [:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,18 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path
-    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -42,5 +38,14 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :quality_id, :burden_fee_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
-end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def not_use_action
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+end
