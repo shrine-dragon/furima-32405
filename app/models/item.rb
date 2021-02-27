b5c14f375cddd1c1a41692a60682b2c980451ab5
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-27 18:52:25 +0900

    itemモデルのスペルを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to       :user
-  has_one          :purechase
+  has_one          :purchase
   has_one_attached :image
   belongs_to       :category 
   belongs_to       :quality 
