80e6ec013c457a6a2e6e86d9493cd5468459fed5
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-27 12:28:20 +0900

    itemsコントローラーとitems/showビューファイルの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item,           only:   [:show, :edit, :update]
-  before_action :move_to_root_path,  only:   [:edit, :update, :destroy]
+  before_action :move_to_root_path,  only:   [:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -36,10 +36,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if user_signed_in? && current_user.id == @item.user_id 
-      item.destroy
-      redirect_to root_path
-    end
+    item.destroy
+    redirect_to root_path
   end
 
   private
