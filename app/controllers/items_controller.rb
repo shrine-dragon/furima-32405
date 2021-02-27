4a9e312cc8c1f746a5be5c02153da820d3ba8d54
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-27 14:38:16 +0900

    itemsコントローラーのdestroyアクションの修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item,           only:   [:show, :edit, :update]
-  before_action :move_to_root_path,  only:   [:edit, :update]
+  before_action :set_item,           only:   [:show, :edit, :update, :destroy]
+  before_action :move_to_root_path,  only:   [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -35,12 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if current_user.id == item.user_id
-      item.destroy
+    if @item.destroy
       redirect_to root_path
-    else
-      render :index
     end
   end
 
