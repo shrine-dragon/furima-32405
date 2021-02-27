3539fcb1cd6f94184ea050c5b36294c3f2d6c170
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-27 17:45:39 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :purchase, only: [:index, :new, :create]
+    resources :purchases, only: [:new, :create]
   end
 end
