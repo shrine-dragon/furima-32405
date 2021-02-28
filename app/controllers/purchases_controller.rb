51904ca794584ab9c49df1b55c0c94c9d379b14e
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-02-28 11:00:54 +0900

    購入画面での商品情報の表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,11 @@
 class PurchasesController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new(purchase_params)
     if @item_purchase.valid?
       @item_purchase.save
