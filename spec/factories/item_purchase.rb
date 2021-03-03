aaed5d703ef69d3c04aa51210c732116ea747587
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-03 19:15:59 +0900

    テストコードの記述

--- spec/factories/item_purchase.rb
+++ spec/factories/item_purchase.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :item_purchase do
-    postal_code   { Faker::Number.number(digits: 7) }
+    token {"tok_abcdefghijk00000000000000000"}    
+    postal_code   { "123-4567" }
     prefecture_id { Faker::Number.between(from: 1, to: 47) }
     city          { Faker::Address.city }
     house_number  { Faker::Address.street_address }
     building_name { Faker::Lorem.word }
     phone_number  { Faker::Number.leading_zero_number(digits: 11) }
-    token {"tok_abcdefghijk00000000000000000"}
   end
 end
---
+++ spec/models/item_purchase_spec.rb
@@ -0,0 +1,82 @@
+require 'rails_helper'
+
+RSpec.describe ItemPurchase, type: :model do
+  before do
+    @item_purchase = FactoryBot.build(:item_purchase)
+  end
+  
+  describe '商品購入機能の実装' do
+    context '商品購入がうまくいく時' do
+      it '全ての値が適切に入力されていれば購入できる' do
+        expect(@item_purchase).to be_valid
+      end
+  
+      it '建物名が入力されていなくても購入できる' do
+        @item_purchase.building_name = ""
+        expect(@item_purchase).to be_valid
+      end
+    end
+  
+    context '商品購入がうまくいかない時' do
+      it 'tokenが空だと購入できない' do
+        @item_purchase.token = ""
+        @item_purchase.valid?
+        expect(@item_purchase.errors.full_messages).to include("Token can't be blank")
+      end
+
+      it 'postal_codeが空だと購入できない' do
+        @item_purchase.postal_code = ""
+        @item_purchase.valid?
+        expect(@item_purchase.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it 'postal_codeが6桁以下だと購入できない' do
+        @item_purchase.postal_code = 123-456
+        @item_purchase.valid?
+        expect(@item_purchase.errors.full_messages).to include("Postal code Input correctly")
+      end
+
+      it 'postal_codeにハイフンが含まれていないと購入できない' do
+        @item_purchase.postal_code = 1234567
+        @item_purchase.valid?
+        expect(@item_purchase.errors.full_messages).to include("Postal code Input correctly")
+      end
+
+      it 'postal_codeに数字以外の値があると購入できない' do
+        @item_purchase.postal_code = "あああaaaアアア"
+        @item_purchase.valid?
+        expect(@item_purchase.errors.full_messages).to include("Postal code Input correctly")
+      end
+
+      it 'prefecture_idが空(--)だと購入できない' do
+        @item_purchase.prefecture_id = 0
+        @item_purchase.valid?
+        expect(@item_purchase.errors.full_messages).to include("Prefecture Select")
+      end
+
+      it 'cityが空だと購入できない' do
+        @item_purchase.city = ""
+        @item_purchase.valid?
+        expect(@item_purchase.errors.full_messages).to include("City can't be blank")
+      end
+
+      it 'house_numberが空だと購入できない' do
+        @item_purchase.house_number = ""
+        @item_purchase.valid?
+        expect(@item_purchase.errors.full_messages).to include("House number can't be blank")
+      end
+
+      it 'phone_numberが空だと購入できない' do
+        @item_purchase.phone_number = ""
+        @item_purchase.valid?
+        expect(@item_purchase.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it 'phone_numberに数字以外の値があると購入できない' do
+        @item_purchase.phone_number = "あああaaaアアア"
+        @item_purchase.valid?
+        expect(@item_purchase.errors.full_messages).to include("Phone number Input only number")
+      end
+    end
+  end
+end
