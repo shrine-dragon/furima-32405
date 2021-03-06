5d96f801b2a8a3fdd7af321fc4a805234d04648e
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-06 19:10:12 +0900

    rubocopを用いて可読性を向上8

--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -19,6 +19,6 @@ class ItemPurchase
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses,
-                             building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+                   building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
