904b3d5b2ab0e280e02d3d263881ac255cc89224
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-06 11:31:59 +0900

    before_actionの記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,13 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :move_to_root_path
+
   def index
-    @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new(purchase_params)
     if @item_purchase.valid?
       pay_item
@@ -18,10 +20,20 @@ class PurchasesController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def purchase_params
     params.require(:item_purchase).permit(:postal_code, :prefecture_id, :city, :addresses, :building_name, :phone_number).merge(
       user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
+  end
+
+  def move_to_root_path
+    if current_user.id == @item.user_id || @item.purchase.present?
+      redirect_to root_path
+    end
   end
 
   def pay_item
