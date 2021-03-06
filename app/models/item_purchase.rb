0ad794b4872038247a9027387ad872047a630f98
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-06 18:36:59 +0900

    item_purchaseモデルの修正2

--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -17,8 +17,8 @@ class ItemPurchase
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'Input correctly' }
 
   def save
-    Purchase.create(user_id: user_id, item_id: item_id)
-    address = Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses,
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses,
                              building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
