b91ebaaf0e159a0fa0e9a5ef2e6012b6e060d482
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-07 08:36:33 +0900

    条件文の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,6 +50,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_root_path
-    redirect_to root_path unless current_user.id == @item.user_id && @item.purchase.blank?
+    redirect_to root_path if (current_user.id != @item.user_id) || @item.purchase.present?
   end
 end
