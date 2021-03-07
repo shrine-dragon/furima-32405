10326e4313b9bdb3cd7acab0526f84c5da41afe7
shrine-dragon <75867325+shrine-dragon@users.noreply.github.com>
2021-03-07 15:04:17 +0900

    テストコードの修正（新規登録パスワード）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,8 +45,8 @@ RSpec.describe User, type: :model do
       end
 
       it 'passwordが5文字以下だと登録できない' do
-        @user.password = 'aaaaa'
-        @user.password_confirmation = 'aaaaa'
+        @user.password = 'abc12'
+        @user.password_confirmation = 'abc12'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
@@ -73,8 +73,8 @@ RSpec.describe User, type: :model do
       end
 
       it 'passwordとpassword_confirmaitonが一致していないと登録できない' do
-        @user.password = 'aaaaaa'
-        @user.password_confirmation = 'aaaaaaaa'
+        @user.password = 'abc123'
+        @user.password_confirmation = 'abc1234'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
@@ -86,7 +86,7 @@ RSpec.describe User, type: :model do
       end
 
       it 'last_nameが半角だと登録できない' do
-        @user.last_name = 'ｱｱｱ'
+        @user.last_name = 'aaaｱｱｱ123'
         @user.valid?
         expect(@user.errors.full_messages).to include('Last name is invalid. Input full-width characters.')
       end
@@ -98,7 +98,7 @@ RSpec.describe User, type: :model do
       end
 
       it 'first_nameが半角だと登録できない' do
-        @user.first_name = 'ｱｱｱ'
+        @user.first_name = 'aaaｱｱｱ123'
         @user.valid?
         expect(@user.errors.full_messages).to include('First name is invalid. Input full-width characters.')
       end
@@ -110,7 +110,7 @@ RSpec.describe User, type: :model do
       end
 
       it 'last_name_kanaが半角だと登録できない' do
-        @user.last_name_kana = 'ｱｱｱ'
+        @user.last_name_kana = 'aaaｱｱｱ123'
         @user.valid?
         expect(@user.errors.full_messages).to include('Last name kana is invalid. Input full-width katakana characters.')
       end
@@ -122,7 +122,7 @@ RSpec.describe User, type: :model do
       end
 
       it 'first_name_kanaが半角だと保存できない' do
-        @user.first_name_kana = 'ｱｱｱ'
+        @user.first_name_kana = 'aaaｱｱｱ123'
         @user.valid?
         expect(@user.errors.full_messages).to include('First name kana is invalid. Input full-width katakana characters.')
       end
